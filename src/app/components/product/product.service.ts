86e4e0946a308ed58c2db4095778f2bf6965704a
Vinicius Bussolo <vinicius.buss13@gmail.com>
2024-04-11 10:35:30 -0300

    :test_tube: test: create test for header.service.ts

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Product } from './product.model';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable, catchError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,29 +13,55 @@ export class ProductService {
 
   constructor(private snakBar: MatSnackBar, private http: HttpClient) {}
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snakBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
       verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
     });
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseURl, product);
+    return this.http.post<Product>(this.baseURl, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.erroHandler(e))
+    );
   }
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseURl)
+    return this.http.get<Product[]>(this.baseURl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.erroHandler(e))
+    );
   }
 
   readById(id: string): Observable<Product> {
-    const url = `${this.baseURl}/${id}`
-    return this.http.get<Product>(url)
+    const url = `${this.baseURl}/${id}`;
+    return this.http.get<Product>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.erroHandler(e))
+    );
   }
 
   update(product: Product): Observable<Product> {
-    const url = `${this.baseURl}/${product.id}`
-    return this.http.put<Product>(url, product)
+    const url = `${this.baseURl}/${product.id}`;
+    return this.http.put<Product>(url, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.erroHandler(e))
+    );
+  }
+
+  delete(id: string): Observable<Product> {
+    const url = `${this.baseURl}/${id}`;
+    return this.http.delete<Product>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.erroHandler(e))
+    );
+  }
+
+  erroHandler(error: any): Observable<any> {
+    this.showMessage('Ocorreu um erro!', true);
+    return EMPTY;
   }
 }
--- src/app/components/template/header/header.component.ts
+++ src/app/components/template/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { HeaderService } from './header.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, RouterModule ],
+  imports: [MatToolbarModule, RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private headerService: HeaderService) {}
 
+  ngOnInit(): void {}
+
+  get title(): string {
+    return this.headerService.headerData.title;
+  }
+
+  get icon(): string {
+    return this.headerService.headerData.icon;
+  }
+
+  get routeUrl(): string {
+    return this.headerService.headerData.routeUrl;
+  }
 }
